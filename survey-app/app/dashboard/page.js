0388d8dafcc5b56ca3893e3d645b7642a204cd48
waeeelhajji <waelhajji.contact@gmail.com>
2024-09-26 11:36:13 +0100

    setup-project

---
+++ survey-app/app/dashboard/page.js
@@ -0,0 +1,13 @@
+import Main from "@/components/Main";
+
+export const metadata = {
+    title: "SurveyApp · Dashboard",
+};
+
+export default function DashboardPage() {
+    return (
+        <Main>
+
+        </Main>
+    )
+}
